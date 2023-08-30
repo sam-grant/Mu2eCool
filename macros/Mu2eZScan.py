410f0ed7f42122737bb14788d5a9afe3119919c6
Samuel Paul Grant <Samuel Grant>
2023-08-30 10:08:05 -0500

    Added Mu2eZScan.py

--- macros/Mu2eZScan.py
+++ macros/Mu2eZScan.py
@@ -2,50 +2,30 @@
 # Rely on a set of 11 ZNTuples spaced around the PT, at 1265:2265:100 mm 
 # Code draws on BeamProfile.py 
 
-
-import uproot
 import pandas as pd
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Internal libraries
 import Utils as ut
 
-from matplotlib.patches import Rectangle
+# from matplotlib.patches import Rectangle
 
 # Globals
 g4blVer="v3.06"
 
-def FilterParticles(df, particle): 
-
-	# Filter particles
-	if particle in particleDict.values():
-		PDGid = list(particleDict.keys())[list(particleDict.values()).index(particle)]
-		df = df[df['PDGid'] == PDGid]
-
-	if particle=="no_proton":
-
-		df = df[df['PDGid'] != 2212]
-
-	if particle=="pi-_and_mu-":
-
-		df = df[(df['PDGid'] == -211) | (df['PDGid'] == 13)]
-
-	return df
-
 from matplotlib.ticker import ScalarFormatter
 import matplotlib.cm as cm
 from matplotlib.colors import ListedColormap
-import math
+# import math
 
-def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatter.png", NDPI=300):
+def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatter.png", NDPI=300, includeBlack=False):
 
 	# Create figure and axes
 	fig, ax = plt.subplots()
 
 	# Create a dictionary to store data for each particle type
-	data_series = {particleName: [] for particleName in particleDict.values()}
+	data_series = {particleName: [] for particleName in ut.particleDict.values()}
 
 	# Extract individual dictonaries from data and populate graphs
 	for i, a in allData.items():
@@ -54,7 +34,7 @@ def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatte
 
     # Define the colourmap colours
 	colours = [
-		# (0., 0., 0.),                                                   # Black
+		(0., 0., 0.),                                                   # Black
 		(0.12156862745098039, 0.4666666666666667, 0.7058823529411765),  # Blue
 		(0.8392156862745098, 0.15294117647058825, 0.1568627450980392),  # Red
 		(0.17254901960784313, 0.6274509803921569, 0.17254901960784313), # Green
@@ -79,15 +59,14 @@ def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatte
 			z, N = zip(*data)
 
 			# Update the labels
-			if label == "proton": label = "$p$"
-			elif label == "pi+": label = "$\pi^{+}$"
-			elif label == "pi-": label = "$\pi^{-}$"
-			elif label == "mu+": label = "$\mu^{+}$"
-			elif label == "mu-": label = "$\mu^{-}$"
+			label = ut.GetLatexParticleName(label)
+
+			colour = cmap(i+1)
+			if includeBlack: colour = cmap(i)
 
 			# Plot
-			ax.scatter(z, N, label=label, marker='o', color=cmap(i), s=4) # , linestyle="-")
-			ax.plot(z, N, label=None, color=cmap(i), linestyle='-')  # Connect points with lines
+			ax.scatter(z, N, label=label, marker='o', color=colour, s=4) # , linestyle="-")
+			ax.plot(z, N, label=None, color=colour, linestyle='-')  # Connect points with lines
 
 	# Add a legend
 	legend = ax.legend(loc="upper left", frameon=False, fontsize=14, bbox_to_anchor=(0.73, 0.99)) 
@@ -103,11 +82,11 @@ def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatte
 	ax.tick_params(axis='y', labelsize=14)  # Set y-axis tick label font size
 
 	# Scientific notation
-	if ax.get_xlim()[1] > 999:
+	if ax.get_xlim()[1] > 9999:
 		ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
 		ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 		ax.xaxis.offsetText.set_fontsize(14)
-	if ax.get_ylim()[1] > 999:
+	if ax.get_ylim()[1] > 9999:
 		ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
 		ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 		ax.yaxis.offsetText.set_fontsize(14)
@@ -170,6 +149,7 @@ def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatte
 	             color='gray', 
 	             fontsize=14,  # Font size of the label text
 	             rotation='vertical')  # Rotate the label
+	# ----- Add tracker and calo if needed ----- 
 	# plt.annotate("Tracker",
 	#              xy=(17964, plt.ylim()[1]),  
 	#              xytext=(10, 10),  
@@ -199,7 +179,7 @@ def PlotGraphOverlay(allData, title=None, xlabel=None, ylabel=None, fout="scatte
 	plt.clf()
 	plt.close()
 
-def Run(config, branchNames, particle, outDir): 
+def Run(config): # , branchNames, particle): 
 
 	# Setup input 
 	finName = "../ntuples/"+g4blVer+"/g4beamline_"+config+".root"
@@ -215,19 +195,19 @@ def Run(config, branchNames, particle, outDir):
 		print("---> Reading", ntupleName)
 
 		# Load TTree into DataFrame
-		df = ut.TTreeToDataFrame(finName, "NTuple/"+ntupleName, branchNames) 
+		df = ut.TTreeToDataFrame(finName, "NTuple/"+ntupleName, ut.branchNames) 
 
 		# Filter upstream particles 
 		# df = df[df['Pz'] > 0]
 
 		# Filter PDGids
-		df = FilterParticles(df, particle)
+		# df = ut.FilterParticles(df, particle)
 
 		# Dictionary for particles at this Z
 		particleN = {}
 
 	    # Loop through particleDict
-		for PDGid, particleName in particleDict.items():
+		for PDGid, particleName in ut.particleDict.items():
 			# Count the occurrences of pdg_id in the DataFrame and add the count to particleN dictionary
 			# if particleName=="proton": continue
 			particleN[particleName] = df[df['PDGid'] == PDGid].shape[0]
@@ -235,35 +215,13 @@ def Run(config, branchNames, particle, outDir):
 		particleNZ[i_z] = particleN
 
 	# Plot particle population as a function of z
-	PlotGraphOverlay(particleNZ, xlabel="z [mm]", ylabel="N / 500 mm", fout="../img/"+g4blVer+"/"+outDir+"/gr_NvsZ_"+config+".png")
+	PlotGraphOverlay(particleNZ, xlabel="z [mm]", ylabel="N / 500 mm", fout="../img/"+g4blVer+"/Mu2eZScan/gr_NvsZ_"+config+".png")
 
 	return
 
 def main():
 
-	branchNames = [ 
-		"x", 
-		"y", 
-		"z", 
-		"Px", 
-		"Py",
-		"Pz",
-		"t",
-		"PDGid",
-		"EventID",
-		"TrackID",
-		"ParentID",
-		"Weight"
-		# extend as needed
-		]  
-
-	particle = "all" # no_proton"
-
-	config="Mu2E_1e7events_ManyZNTuple2_fromZ2265_parallel"
-	outDir="BeamFlux"
-
-	Run(config, branchNames, particle, outDir)
-
+	Run("Mu2E_1e7events_ManyZNTuple2_fromZ2265_parallel") 
 
 if __name__ == "__main__":
 	main()
