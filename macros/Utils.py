332d8a4f2fa28e75e8b9f631099d7052fb9dfa8c
Samuel Paul Grant <Samuel Grant>
2023-08-30 10:34:18 -0500

    Finished /macros rewrite

--- macros/Utils.py
+++ macros/Utils.py
@@ -90,13 +90,26 @@ def TTreeToDataFrame(finName, treeName, branchNames):
 
     print("---> Reading", treeName, "in", finName)
 
-    # Open the ROOT file
+    # # Check the file has keys available
+    # try:
+    #     with uproot.open(finName) as fin:
+    #         # Check if the file contains any keys (objects)
+    #         if not bool(fin.keys()):
+    #             print("Warning: file", finName, "has no keys!")
+
+    #         else:
+
+    # Open the file            
     fin = uproot.open(finName)
     
     # print("---> Got input file ", finName, ", ", (fin))
 
     # Get tree
     tree = fin[treeName]
+
+    if len(tree) == 0: 
+        
+        return
 
     # print("---> Got tree ", str(tree))
 
@@ -485,6 +498,106 @@ def Plot1DOverlayWithStats(hists, nBins=100, xmin=-1.0, xmax=1.0, title=None, xl
     print("---> Written", fout)
 
     # Clear memory
+    plt.close()
+
+# Input is a dictionary with four lists per key
+# Lists are x, xerr, y, err; key is the label
+def PlotGraphOverlay(graph_dict, title=None, xlabel=None, ylabel=None, fout="scatter.png", NDPI=300):
+
+    # Create a scatter plot with error bars using NumPy arrays 
+
+    # Create figure and axes
+    fig, ax = plt.subplots()
+
+    # Define the colourmap colours
+    # colours = [
+    #   # (0., 0., 0.),                                                   # Black
+    #   (0.12156862745098039, 0.4666666666666667, 0.7058823529411765),  # Blue
+    #   (0.8392156862745098, 0.15294117647058825, 0.1568627450980392),  # Red
+    #   (0.17254901960784313, 0.6274509803921569, 0.17254901960784313), # Green
+    #   (1.0, 0.4980392156862745, 0.054901960784313725),                # Orange
+    #   (0.5803921568627451, 0.403921568627451, 0.7411764705882353),    # Purple
+    #   (0.09019607843137255, 0.7450980392156863, 0.8117647058823529),   # Cyan
+    #   (0.8901960784313725, 0.4666666666666667, 0.7607843137254902),   # Pink
+    #   (0.5490196078431373, 0.33725490196078434, 0.29411764705882354), # Brown
+    #   (0.4980392156862745, 0.4980392156862745, 0.4980392156862745),   # Gray 
+    #   (0.7372549019607844, 0.7411764705882353, 0.13333333333333333)  # Yellow
+    # ]
+
+    # Create the colormap
+    # cmap = ListedColormap(colours)
+    colour_idx = 0
+
+    cmap = cm.get_cmap('tab10') # !!deprecated!!
+
+    print(graph_dict)
+
+    # Iterate through the keys (momentum values) in the dictionary
+    for label, graph_list in graph_dict.items():
+
+        for graph in graph_list:
+
+            print(graph)
+
+            print("\n", label, graph)
+
+            # print("HHHHEEELLLOOOO")
+
+            x = graph['x']
+            y = graph['y']
+            xerr = graph['xerr'] 
+            yerr = graph['yerr']
+
+            print(x,y)
+
+            # Plot scatter with error bars
+            # if len(xerr)==0: xerr = [0] * len(x) # Sometimes we only use yerr
+            # if len(yerr)==0: yerr = [0] * len(y) # Sometimes we only use yerr
+
+            ax.errorbar(x=x, y=y, xerr=xerr, yerr=yerr, fmt='o', color=cmap(colour_idx), markersize=4, ecolor=cmap(colour_idx), capsize=2, elinewidth=1, linestyle='None', label=str(label)+" MeV")
+
+            colour_idx += 1
+
+    # Add a line at 30 MeV
+
+    # Set title, xlabel, and ylabel
+    ax.set_title(title, fontsize=16, pad=10)
+    ax.set_xlabel(xlabel, fontsize=14, labelpad=10) 
+    ax.set_ylabel(ylabel, fontsize=14, labelpad=10) 
+
+    # Set font size of tick labels on x and y axes
+    ax.tick_params(axis='x', labelsize=14)  # Set x-axis tick label font size
+    ax.tick_params(axis='y', labelsize=14)  # Set y-axis tick label font size
+
+    legend = ax.legend(loc="center left", frameon=False, fontsize=14, bbox_to_anchor=(1, 0.5)) 
+    # Add a legend on the right side outside the plot
+    # plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    # Adjust the figure size to make space for the legend
+    # plt.gcf().set_size_inches(8, 5)  # Adjust the width and height as needed
+
+    # plt.xlim(xmin=-10) # , xmax=110)
+
+
+    # legend = ax.legend(loc="upper right", frameon=False, fontsize=14, bbox_to_anchor=(0.73, 0.99)) 
+    # Scientific notation
+    if ax.get_xlim()[1] > 9999:
+        ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        ax.xaxis.offsetText.set_fontsize(14)
+    if ax.get_ylim()[1] > 9999:
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax.yaxis.offsetText.set_fontsize(14)
+
+
+
+    # Save the figure
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
+
+    # Clear memory
+    plt.clf()
     plt.close()
 
 def Plot2DWith1DProj(x, y, nBinsX=100, xmin=-1.0, xmax=1.0, nBinsY=100, ymin=-1.0, ymax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", NDPI=300):
