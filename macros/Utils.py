64d2b9076ea01548d2392f3e4f61cfb2b897a978
Samuel Paul Grant <Samuel Grant>
2023-09-19 10:52:24 -0500

    Added ratio plot function to Utils

--- macros/Utils.py
+++ macros/Utils.py
@@ -987,4 +987,125 @@ def BarChart(data, label_dict, title=None, xlabel=None, ylabel=None, fout="bar_c
     plt.clf()
     plt.close()
 
+def Plot1DRatio(hists, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", labels=None, legPos="best", stats=False, errors=False, NDPI=300, peak=False):
+    
+    if len(hists) > 2: 
+        print("!!! ERROR: Plot1DRatio must take two histograms as input !!!")
+        return
 
+    # Create figure and axes
+    fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]}, figsize=(8, 6))
+
+    # Define a colormap
+    colours = [
+        (0.12156862745098039, 0.4666666666666667, 0.7058823529411765),  # Blue
+        (0.8392156862745098, 0.15294117647058825, 0.1568627450980392),  # Red
+    ]
+
+    # Create the colormap
+    cmap = ListedColormap(colours)
+
+    counts_ = []
+
+    # Iterate over the histograms and plot each one in the top frame
+    for i, hist in enumerate(hists):
+
+        colour = cmap(i)
+
+        # Calculate statistics for the current histogram
+        N, mean, meanErr, stdDev, stdDevErr, underflows, overflows = GetBasicStats(hist, xmin, xmax)
+
+        # Create legend text
+        legend_text = f"Entries: {N}\nMean: {Round(mean, 3)}\nStd Dev: {Round(stdDev, 3)}"
+        if errors:
+            legend_text = f"Entries: {N}\nMean: {Round(mean, 4)}$\pm${Round(meanErr, 1)}\nStd Dev: {Round(stdDev, 3)}$\pm${Round(stdDevErr, 1)}"
+        if peak and not errors:
+            legend_text += f"\nPeak: {Round(GetMode(hist, nbins / (xmax - xmin))[0], 3)}"
+        if peak and errors:
+            legend_text += f"\nPeak: {Round(GetMode(hist, nbins / (xmax - xmin))[0], 3)}$\pm${Round(GetMode(hist, nbins / (xmax - xmin))[1], 1)}"
+
+        if stats:
+            label = r"$\bf{"+labels[i]+"}$"+"\n"+legend_text
+        else:
+            label = labels[i]
+
+        # Plot the current histogram in the top frame
+        counts, bin_edges, _ = ax1.hist(hist, bins=nbins, range=(xmin, xmax), histtype='step', edgecolor=colour, linewidth=1.0, fill=False, density=False, color=colour, label=label) 
+
+        counts_.append(counts)
+
+    # Calculate the ratio of the histograms with a check for division by zero
+    ratio = np.divide(counts_[0], counts_[1], out=np.full_like(counts_[0], np.nan), where=(counts_[1] != 0))
+
+    # Calculate the statistical uncertainty for the ratio
+    ratio_err = np.divide(np.sqrt(counts_[0]), counts_[1], out=np.full_like(counts_[0], np.nan), where=(counts_[1] != 0))
+
+    # Create a second y-axis for the ratio
+    # ax2 = ax1.twinx() # This overlays them
+    # Create a separate figure and axis for the ratio plot
+    # fig2, ax2 = plt.subplots(figsize=(8, 2))  # Adjust the height as needed
+
+    # Add line at 1.0 
+    ax2.axhline(y=1.0, color='gray', linestyle='--', linewidth=1)
+
+    # Set x-axis limits for the ratio plot to match the top frame
+    ax2.set_xlim(xmin, xmax)
+
+    # Plot the ratio in the lower frame with error bars
+    ax2.errorbar(bin_edges[:-1], ratio, yerr=ratio_err, color='black', fmt='o', markersize=4, linewidth=1)
+
+    # # Plot the ratio in the lower frame
+    # ax2.plot(bin_edges[:-1], ratio, color='black', marker='o', markersize=4, linewidth=0)
+
+    # Format 
+
+    # Set x-axis limits for the top frame
+    ax1.set_xlim(xmin, xmax)
+    # Remove markers for main x-axis
+    ax1.set_xticks([])
+
+    ax2.set_xlabel(xlabel, fontsize=14, labelpad=10)
+    ax2.set_ylabel("Ratio", fontsize=14, labelpad=10)
+    ax2.tick_params(axis='x', labelsize=14)
+    ax2.tick_params(axis='y', labelsize=14)
+
+    # Create a second y-axis for the ratio
+    ax2.yaxis.tick_left()
+    ax2.xaxis.tick_bottom()
+    ax2.xaxis.set_tick_params(width=0.5)
+    ax2.yaxis.set_tick_params(width=0.5)
+
+    # Set titles and labels for both frames
+    ax1.set_title(title, fontsize=16, pad=10)
+    # ax1.set_xlabel("", fontsize=0, labelpad=10)
+    ax1.set_ylabel(ylabel, fontsize=14, labelpad=10)
+
+    # Set font size of tick labels on x and y axes for both frames
+    # ax1.tick_params(axis='x', labelsize=14)
+    ax1.tick_params(axis='y', labelsize=14)
+
+    # Scientific notation for top frame
+    if ax2.get_xlim()[1] > 9999:
+        ax2.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax2.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        ax2.xaxis.offsetText.set_fontsize(14)
+    if ax1.get_ylim()[1] > 9999:
+        ax1.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax1.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax1.yaxis.offsetText.set_fontsize(14)
+
+    # Add legend to the top frame
+    ax1.legend(loc=legPos, frameon=False, fontsize=14)
+
+    # Adjust the spacing between subplots
+    plt.tight_layout()
+
+    # Adjust the spacing between subplots to remove space between them
+    plt.subplots_adjust(hspace=0.0)
+
+    # Save the figure
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
+
+    # Clear memory
+    plt.close()
